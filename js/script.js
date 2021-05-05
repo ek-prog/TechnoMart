18d4befd6229d7a7d2df85868caea328a99b38c8
ek-prog <Fedorova38@icloud.com>
2021-05-05 22:01:27 +0800

    corrected file

--- js/script.js
+++ js/script.js
@@ -12,57 +12,33 @@ function closeForm(el) {
 
 //Слайдер
 
-// let slides = document.querySelectorAll('.item_slide');
-// let currentSlide = 0; //создали переменную для текущего слайда 
-// let slideInterval = setInterval(nextSlide, 3000); // создали интервал для следующего слайда в 3 сек
 
-// function nextSlide() {
-// 	goToSlide(currentSlide + 1); //вперед
+
+
+// let slideIndex = 0;
+// showSlides();
+
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
 // }
-// function previousSlide() {
-// 	goToSlide(currentSlide - 1); //назад
+
+// function showSlides() {
+//     let i;
+//     let slides = document.getElementsByClassName("item_slide");
+//     let dots = document.getElementsByClassName("dot");
+//     for (i = 0; i < slides.length; i++) {
+//        slides[i].style.display = "none";  
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}    
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";  
+//     dots[slideIndex-1].className += " active";
+//     setTimeout(showSlides, 2000); // Change image every 2 seconds
 // }
-// function goToSlide(n) {
-// 	slides[currentSlide].className = 'item_slide';// меняем класс для текущего слайда 
-// 	currentSlide = (n + slides.length) % slides.length;
-// 	slides[currentSlide].className = 'item_showing'; //меняем текущему слайду класс на active
-
-// }
-// let nextBtn = document.getElementById('next_btn');
-// let previousBtn = document.getElementById('previous_btn');
-
-// nextBtn.onclick = function () {
-// 	nextSlide();
-// };
-// previousBtn.onclick = function () {
-// 	previousSlide();
-// };
-
-
-let slideIndex = 0;
-showSlides();
-
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("item_slide");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
 
 
 
@@ -75,29 +51,7 @@ function showSlides() {
 
 //Слайдер производителей
 
-// let slidesManuf = document.querySelectorAll('#slides_manuf .slide');
-// let currentSlideManuf = 0;
-// let slideIntervalManuf = setInterval(nextSlideManuf, 2200);
 
-// function nextSlideManuf() {
-// 	if (document.body.clientWidth < 640) {
-// 		slidesManuf[currentSlideManuf].className = 'slide';
-// 		currentSlideManuf = (currentSlideManuf + 1) % slidesManuf.length;
-// 		slidesManuf[currentSlideManuf].className = 'slide showing'
-// 	}
-// }
-
-// let link = document.querySelectorAll('.header_navigation_item li');
-
-// for (let i = 0; i < link.length; i++) {
-// 	link[i].addEventListener("click", function () {
-// 		let currentLink = document.getElementsByClassName('focus');
-// 		currentLink[0].className = currentLink[0].className.replace(" focus", "");
-// 		this.className += " focus";
-
-// 	});
-// }
-// console.log(link);
 
 
 function myFunction(el){
@@ -121,15 +75,38 @@ document.querySelector('.button_second').onclick = function(){
 // 	}
 // }
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   // инициализация слайдера
-//   new SimpleAdaptiveSlider('.slider', {
-//     loop: true,
-//     autoplay: true,
-//     interval: 3000,
-//     swipe: true,
-//   });
-// });
+$(document).ready(function(){
+	$('.item_category_slider').slick({
+		arrows: false,
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 1000,
+		easing: 'ease',
+		infinite: true,
+		initialSlide: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		draggable: true,
+		swipe: true,
+		touchThreshold:10,
+		touchMove: true
+
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
 	$('.slider').slick({
 		arrows: true,
